ce5008faa45d5256944177d95a804ced632321fc
Jenie Nam <jenien@uci.edu>
2020-05-07 12:26:05 -0700

    hid API keys

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "f6d660510aef7918d339d034551ee989"
+weather_api_key = ""
 
 # Google API Key
-g_key = "AIzaSyBHJAViXrFfbnarBmZ64B-qUHoYPW--k2Q"
+g_key = ""
