bdeaaebbe1150c31530a13d8cee8236bdf14a429
Jenie Nam <jenien@uci.edu>
2020-05-07 12:21:10 -0700

    Worked on Homework

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "f6d660510aef7918d339d034551ee989"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyBHJAViXrFfbnarBmZ64B-qUHoYPW--k2Q"
